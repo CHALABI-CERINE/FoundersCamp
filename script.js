00027ab1bed8aae6443982bcb1b1de7ead612418
CHALABI-CERINE <chalabicerinemaria@gmail.com>
2025-12-02 21:54:09 +0100

    lien form

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const timer = setInterval(function() {
 
 // --- GESTION DU FORMULAIRE VERS GOOGLE SHEET ---
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycby1ATKDhEv35gICjqxPZekPznfOMezS1DCV0b17IA3anL8ro8tDtZkT9vwHet65z2Skyw/exec'; 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzULulGhYoi6VJj6QG8o9QjI0Gfw4qW1y5rzcuy1ubFGsuaR3LZ2Xu0wcnbgZHeDjtQyQ/exec'; 
 
 const form = document.forms['google-sheet'];
 const msg = document.getElementById("msg");
